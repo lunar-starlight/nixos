7400fe1aacf1984a12ce12954d536055ebe013b4
Luna Strah <strah.luna@gmail.com>
2024-11-01 16:10:42 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "NixOS config flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }@inpus: {
+    nixosConfigurations.pink-pear = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.muf = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
