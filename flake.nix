bb835a11695a4781389334289bea216db450ed24
Luna Strah <45151812+lunar-starlight@users.noreply.github.com>
2024-12-06 20:38:00 +0100

    added lix

--- flake.nix
+++ flake.nix
@@ -3,17 +3,23 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3-1.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, ... }@inputs:
+  outputs = { nixpkgs, lix-module, ... }@inputs: {
   let
-    hostname = "rainbow-lemon";
-  in {
+    hostname = "pink-pear";
+  in 
     nixosConfigurations.${hostname} = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit hostname; };
       modules = [
         ./configuration.nix
+        lix-module.nixosModules.default
       ];
     };
   };
