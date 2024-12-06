bcc4e1ed76a75662e7680c279f0182f3f03a6883
Luna Strah <45151812+lunar-starlight@users.noreply.github.com>
2024-12-06 09:50:28 +0100

    upgrade nix

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "NixOS config flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
   outputs = { nixpkgs, ... }@inputs: {
-    nixosConfigurations.pink-pear = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.rainbow-lemon = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
