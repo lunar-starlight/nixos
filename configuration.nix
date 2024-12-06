1a82589057d72431a03c800eae69031b96d7086e
Luna Strah <45151812+lunar-starlight@users.noreply.github.com>
2024-12-06 09:51:59 +0100

    set up laptop hardware config and hostname

--- configuration.nix
+++ configuration.nix
@@ -9,7 +9,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "pink-pear";
+  networking.hostName = "rainbow-lemon";
   networking.networkmanager.enable = true;
 
   time.timeZone = "Europe/Ljubljana";
