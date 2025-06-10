5b49d6e05aa00637f459e3367b11c2cef540b724
Luna Strah <45151812+lunar-starlight@users.noreply.github.com>
2025-06-10 17:57:51 +0200

    upgrade to 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS config flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
   outputs = { nixpkgs, ... }@inputs: {
